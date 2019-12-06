6d1679c8fa5bdf25ce6fc40c09f27b4f155329cc
Pino <hgy44@qq.com>
2019-12-06 16:01:45 +0800

    初步创建了大厅和昵称设置

---
+++ SustechMahjong/Assets/scrips/GameConcroller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConcroller : MonoBehaviour
+{
+    public void setNameButtonPress()
+    {
+        //跳转到大厅
+        Application.LoadLevel("hall");
+    }
+}
