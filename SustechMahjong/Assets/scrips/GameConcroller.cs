6238a62d208f9e5d8c67329ee84e9f2ae139184e
Pino <hgy44@qq.com>
2019-12-06 20:39:55 +0800

    完善了大厅界面，加入了设置和介绍按钮

--- SustechMahjong/Assets/scrips/GameConcroller.cs
+++ SustechMahjong/Assets/scrips/GameConcroller.cs
@@ -1,12 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameConcroller : MonoBehaviour
 {
+    public InputField nickName;
+    public Text remind;
     public void setNameButtonPress()
     {
-        //跳转到大厅
-        Application.LoadLevel("hall");
+        //判断昵称后跳转到大厅
+        String nickName = this.nickName.text;
+        if (nickName == "")
+        {
+            //昵称是空的，提示用户
+            print("name is none");
+            remind.gameObject.SetActive(true);
+            StartCoroutine(killReminder());
+        }
+        else
+        {
+            //进入大厅
+            PlayerPrefs.SetString("nickName",nickName);
+            Application.LoadLevel("hall");
+        }
+    }
+
+    IEnumerator killReminder()
+    {
+        yield return new WaitForSeconds(1);
+        remind.gameObject.SetActive(false);
     }
 }
---
+++ SustechMahjong/Assets/scrips/hall/getName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getName : MonoBehaviour
+{
+    public Text name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        String nickName = PlayerPrefs.GetString("nickName");
+        name.text = nickName;
+    }
+    
+}
