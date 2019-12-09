3ebadee521ce7db901e7eb74ddb2b4d796d9ce12
pino <11710807@mail.sustc.edu.cn>
2019-12-09 20:55:19 +0800

    加入了选择导师的界面

---
+++ SustechMahjong/Assets/scrips/room/MentorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MentorController : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+
+{
+    public GameObject text;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.SetActive(true);
+        Debug.Log("enter!");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.SetActive(false);
+        Debug.Log("exit!");
+    }
+    
+}
