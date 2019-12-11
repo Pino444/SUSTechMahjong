2ead7accbdc64c784d2d49a2ed89e49c047b51e1
Pino <hgy44@qq.com>
2019-12-11 23:04:12 +0800

    完善了倒计时器

---
+++ SustechMahjong/Assets/scrips/room/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public float Times=10;
+    public GameObject timerText;
+    public GameObject timer;
+    private Text _text;
+
+    void Start()
+    {
+        _text = timerText.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Times -= Time.deltaTime;
+        _text.text = "" + (int) Times;
+        if (Times < 0)
+        {
+            timer.SetActive(false);
+        }
+    }
+}
