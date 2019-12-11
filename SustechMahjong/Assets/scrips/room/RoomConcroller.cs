8b22e42043ef1356f6fdfb6c07f2c9d1a8ba8967
pino <11710807@mail.sustc.edu.cn>
2019-12-11 15:57:27 +0800

    加入了倒数计时

--- SustechMahjong/Assets/scrips/room/RoomConcroller.cs
+++ SustechMahjong/Assets/scrips/room/RoomConcroller.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RoomConcroller : MonoBehaviour
 {
     public GameObject quitRoomButton;
     public GameObject quitRoomPanel;
     public GameObject chooseMentorPanel;
-
+    public GameObject chooseCourseButton;
+    
     public void onQuitRoomButtonClick()
     {
         quitRoomPanel.SetActive(true);
@@ -27,5 +30,11 @@ public class RoomConcroller : MonoBehaviour
     public void onMentorClick()
     {
         chooseMentorPanel.SetActive(false);
+    }
+
+    public void onScoreValueChange(float value)
+    {
+        print(value);
+        chooseCourseButton.GetComponent<Text>().text = "投入的积分是：\n"+Convert.ToString((int)value);
     }
 }
