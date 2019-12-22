15ffd1ef9412b027e1e0a07319735f23da628b73
Pino <hgy44@qq.com>
2019-12-22 22:15:40 +0800

    Timer可以正确的发消息

--- SustechMahjong/Assets/scrips/room/Timer.cs
+++ SustechMahjong/Assets/scrips/room/Timer.cs
@@ -12,9 +12,11 @@ public class Timer : MonoBehaviour
     public GameObject timerText;
     public GameObject timer;
     private Text _text;
+    public GameObject networkManeger;
     private int statement = -1;//1: 打牌超时； 2 选导师超时； 3 投积分超时 4 吃碰杠胡超时 5 积分选牌超时
     void Start()
     {
+        networkManeger = GameObject.Find("Network");
         _text = timerText.GetComponent<Text>();
         Times = 10;
     }
@@ -31,6 +33,62 @@ public class Timer : MonoBehaviour
         _text.text = "" + (int) Times;
         if (Times < 0)
         {
+            switch (statement)
+            {
+                case 1:
+                    networkManeger.GetComponent<NetworkManeger>().sendMsg(new Dictionary<string, string>()
+                    {
+                        {"type", "playcard"},
+                        {"socket_id", PlayerPrefs.GetString("socket_id")},
+                        {"room",PlayerPrefs.GetString("room")},
+                        {"room_id",PlayerPrefs.GetString("room_id")},
+                        {"content","-1"}
+                    });
+                    break;
+                case 2://TODO
+                    networkManeger.GetComponent<NetworkManeger>().sendMsg(new Dictionary<string, string>()
+                    {
+                        {"type", "supervisor"},
+                        {"socket_id", PlayerPrefs.GetString("socket_id")},
+                        {"room",PlayerPrefs.GetString("room")},
+                        {"room_id",PlayerPrefs.GetString("room_id")},
+                        {"content","1"}
+                        
+                    });
+                    break;
+                case 3:
+                    networkManeger.GetComponent<NetworkManeger>().sendMsg(new Dictionary<string, string>()
+                    {
+                        {"type", "score"},
+                        {"socket_id", PlayerPrefs.GetString("socket_id")},
+                        {"room",PlayerPrefs.GetString("room")},
+                        {"room_id",PlayerPrefs.GetString("room_id")},
+                        {"content","1"}
+                    });
+                    break;
+                case 4:
+                    networkManeger.GetComponent<NetworkManeger>().sendMsg(new Dictionary<string, string>()
+                    {
+                        {"type", "opereply"},
+                        {"socket_id", PlayerPrefs.GetString("socket_id")},
+                        {"room",PlayerPrefs.GetString("room")},
+                        {"room_id",PlayerPrefs.GetString("room_id")},
+                        {"content","0"}
+                    });
+                    break;
+                case 5://TODO
+//                    networkManeger.GetComponent<NetworkManeger>().sendMsg(new Dictionary<string, string>()
+//                    {
+//                        {"type", "quitroom"},
+//                        {"socket_id", PlayerPrefs.GetString("socket_id")},
+//                        {"room",PlayerPrefs.GetString("room")},
+//                        {"room_id",PlayerPrefs.GetString("room_id")}
+//                    });
+                    break;
+                default:
+                    Debug.Log("No such Statement!");
+                    break;
+            }
             timer.SetActive(false);
         }
     }
